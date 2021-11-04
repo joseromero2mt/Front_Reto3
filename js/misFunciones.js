fd634dfc0f67959b701fed56b9c14289f28242f3
joseromero2mt <jose.romero2.mt@correo.usa.edu.co>
2021-11-04 17:42:41 -0500

    first commit

---
+++ js/misFunciones.js
@@ -0,0 +1,182 @@
+
+function traerInformacionCategorias(){
+    $.ajax({
+        url:"http://localhost:8080/api/Category/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta(respuesta);
+        }
+    });
+}
+
+function pintarRespuesta(respuesta){
+
+    let myTable="<table>";
+    for(i=0;i<respuesta.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].name+"</td>";
+        myTable+="<td>"+respuesta[i].description+"</td>";
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#resultado1").html(myTable);
+}
+
+function guardarInformacionCategorias(){
+    let var2 = {
+        name:$("#Cname").val(),
+        description:$("#Cdescription").val()
+        };
+      
+        $.ajax({
+        type:'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var2),
+        
+        url:"http://localhost:8080/api/Category/save",
+       
+        
+        success:function(response) {
+                console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente");
+            window.location.reload()
+    
+        },
+        
+        error: function(jqXHR, textStatus, errorThrown) {
+              window.location.reload()
+            alert("No se guardo correctamente");
+    
+    
+        }
+        });
+
+}
+
+///////////////////Bikes//////////////////////////////////////
+function traerInformacionFarm(){
+    $.ajax({
+        url:"http://localhost:8080/api/Farm/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuestaFarm(respuesta);
+        }
+    });
+}
+
+function pintarRespuestaFarm(respuesta){
+
+    let myTable="<table>";
+    for(i=0;i<respuesta.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].address+"</td>";
+        myTable+="<td>"+respuesta[i].extension+"</td>";
+        myTable+="<td>"+respuesta[i].name+"</td>";
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#resultado2").html(myTable);
+}
+
+function guardarInformacionFarm(){
+    let var3 = {
+        address:$("#Faddress").val(),
+        extension:$("#Fextension").val(),
+        name:$("#Fname").val(),
+        };
+      
+        $.ajax({
+        type:'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var3),
+        
+        url:"http://localhost:8080/api/Bike/save",
+       
+        
+        success:function(response) {
+                console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente");
+            window.location.reload()
+    
+        },
+        
+        error: function(jqXHR, textStatus, errorThrown) {
+              window.location.reload()
+            alert("No se guardo correctamente");
+    
+    
+        }
+        });
+
+}
+//////////////////////Clientes//////////////////////////////////
+function traerInformacionClientes(){
+    $.ajax({
+        url:"http://localhost:8080/api/Client/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuestaClientes(respuesta);
+        }
+    });
+}
+
+function pintarRespuestaClientes(respuesta){
+
+    let myTable="<table>";
+    for(i=0;i<respuesta.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].email+"</td>";
+        myTable+="<td>"+respuesta[i].password+"</td>";
+        myTable+="<td>"+respuesta[i].name+"</td>";
+        myTable+="<td>"+respuesta[i].age+"</td>";
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#resultado3").html(myTable);
+}
+
+function guardarInformacionClientes(){
+    let var4 = {
+        email:$("#CLemail").val(),
+        password:$("#CLpassword").val(),
+        name:$("#CLname").val(),
+        age:$("#CLage").val(),
+        };
+      
+        $.ajax({
+        type:'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var4),
+        
+        url:"http://localhost:8080/api/Client/save",
+       
+        
+        success:function(response) {
+                console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente");
+            window.location.reload()
+    
+        },
+        
+        error: function(jqXHR, textStatus, errorThrown) {
+              window.location.reload()
+            alert("No se guardo correctamente");
+    
+    
+        }
+        });
+
+}
+
